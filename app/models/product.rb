467aac69996984ffa02777937e6a9a5c6e0acd7e
mjennings754 <mitchellj@outlook.jp>
2026-01-17 17:31:05 +1100

    add tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,15 @@
 class Product < ApplicationRecord
   has_one_attached :image
   after_commit -> { broadcast_refresh_later_to "products" }
+  validates_presence_of :title, :description, :image
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+  validate :acceptable_image
+  def acceptable_image
+    return unless image.attached?
+    acceptable_types = ["image/gif", "image/jpeg", "image/png"]
+    unless acceptable_types.include?(image.content_type)
+      errors.add(:image, "must be a GIF, JPG or PNG image")
+    end
+  end
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image].any?
+  end
+
+  test "product price must be positive" do
+    product = Product.new(title: "Acme Book",
+                          description: "acme_description")
+    product.image.attach(io: File.open("test/fixtures/files/box.png"),
+                         filename: "box.png", content_type: "image/jpeg")
+
+    product.price = -1
+    assert product.invalid?
+    assert_equal [ "must be greater than or equal to 0.01"],
+    product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal [ "must be greater than or equal to 0.01"],
+    product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
 end

fcff060d3c526700360cf2c5fc06747c8a7a1894
mjennings754 <mitchellj@outlook.jp>
2026-01-18 09:15:09 +1100

    customer can click checkout

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,7 @@
 class OrdersController < ApplicationController
+  include CurrentCart
+  before_action :set_cart, only: %i[ new create ]
+  before_action :ensure_cart_isnt_empty, only: %i[ new ]
   before_action :set_order, only: %i[ show edit update destroy ]
 
   # GET /orders or /orders.json
@@ -61,6 +64,12 @@ class OrdersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_order
       @order = Order.find(params.expect(:id))
+    end
+
+    def ensure_cart_isnt_empty
+      if @cart.line_items.empty?
+        redirect_to store_index_url, notice: "Your cart is empty"
+      end
     end
 
     # Only allow a list of trusted parameters through.

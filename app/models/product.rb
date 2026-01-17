ee1af70755aca483f556f149fb34c7419cc1018e
mjennings754 <mitchellj@outlook.jp>
2026-01-18 01:10:55 +1100

    add cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   has_one_attached :image
+  has_many :line_items
   after_commit -> { broadcast_refresh_later_to "products" }
   validates_presence_of :title, :description, :image
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
@@ -11,6 +12,15 @@ class Product < ApplicationRecord
     acceptable_types = ["image/gif", "image/jpeg", "image/png"]
     unless acceptable_types.include?(image.content_type)
       errors.add(:image, "must be a GIF, JPG or PNG image")
+    end
+  end
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, "Line Items present")
+      throw :abort
     end
   end
 end

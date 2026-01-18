640c7b0a671346c0d99af3a4964e7005e737088f
mjennings754 <mitchellj@outlook.jp>
2026-01-19 09:28:52 +1100

    cannot delete last user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,17 @@ class User < ApplicationRecord
   validates_presence_of :name
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  after_destroy :ensure_an_admin_remains
+
+  class Error < StandardError
+  end
+
+  private
+
+  def ensure_an_admin_remains
+    if User.count.zero?
+      raise Error.new "Can't delete last user"
+    end
+  end
 end

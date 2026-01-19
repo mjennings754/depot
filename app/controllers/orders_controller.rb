1335cc31df9a58afecddbfb9357afc144d54918b
mjennings754 <mitchellj@outlook.jp>
2026-01-20 09:03:50 +1100

    can switch locale from header and added more translatons

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -33,7 +33,7 @@ class OrdersController < ApplicationController
         session[:cart_id] = nil
         ChargeOrderJob.perform_later(@order, pay_type_params.to_h)
         OrderMailer.received(@order).deliver_later
-        format.html { redirect_to @order, notice: "Thank you for your order." }
+        format.html { redirect_to store_index_url(locale: I18n.locale), notice: I18n.t(".thanks") }
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new, status: :unprocessable_entity }
